4aff5a461963d21536c5aa0626188d52d5a72aff
Ollie Murphy <olivermurphy@Olivers-MacBook-Pro.local>
2014-07-13 19:16:24 +0100

    FirstDocs

---
+++ employer.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>My Job</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Loading Bootstrap -->
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+<!-- Loading Flat UI -->
+<link href="css/flat-ui.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+<!--[if lt IE 9]>
+  <script src="js/html5shiv.js"></script>
+  <script src="js/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+   <div class="navbar navbar-inverse no-radius" role="navigation">
+      <div class="min-width-760 container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">MyJob</a>
+        </div>
+      </div>
+    </div>
+	  				
+		  <div class="container margin-bottom-30 margin-top-30">
+	          <div style="height:700px;" class="group-contain border-radius-3 padding-40 clouds clearfix">
+		         <div class="row">
+			        <div class="col-md-3 margin-left-20  margin-bottom-30">
+			         	<div style="height:200px;width:200px" class="border-radius-10 offwhite margin-left-20">
+			         		<img width="200px" src="images/profile_icon.png">
+			         	</div>
+			        </div>
+			        <div class="col-md-2 margin-left-20">
+				        <h5>John Doe</h5>
+			        </div>
+			       
+			        <div style="height:200px" class="col-md-6 margin-left-20 offwhite margin-left-20 border-radius-5 faint-shadow border-white">
+				          <div class="col-md-12 text-center margin-top-10 margin-bottom-10 border-bottom-1">
+				         	 <p><span class="fui-list margin-right-20"></span>My Profile</p>
+				          </div>
+				          <div class="col-md-5 col-md-offset-2">
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-check margin-right-20"></span>Employee Matches</a></p>
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-image margin-right-20"></span>View Profile</a></p>
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-gear margin-right-20"></span>Profile Settings</a></p>
+				          </div>
+				          <div class="col-md-5">
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-check margin-right-20"></span>Job Matches</a></p>
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-image margin-right-20"></span>View Profile</a></p>
+						 	 <p class="font-14 margin-bottom-10"><a href="#"><span class="fui-gear margin-right-20"></span>Profile Settings</a></p>
+				          </div>				          
+					</div><!----------END OF MY PROFILE----------->			        
+		         </div><!-----------END OF ROW ----------->
+		         
+		         
+		          <div class="row">
+			          <div style="height:400px" class="col-md-3 margin-left-20 offwhite border-radius-5 faint-shadow border-white">
+				         <div class="col-md-12 text-center margin-top-10 margin-bottom-20 border-bottom-1">
+				          		<p><span class="fui-search margin-right-20"></span>Quick Search</p>
+				         </div>       
+						 <div class="form-group">
+							  <input type="text" value="" placeholder="Job Title" class="form-control input-sm">
+						 </div>
+						 <div class="form-group margin-top-20">
+							  <input type="text" value="" placeholder="Keywords" class="form-control input-sm">
+						 </div>
+				        <div class="col-md-6">
+							<label class="checkbox" for="checkbox1">
+								<span class="icons">
+									<span class="first-icon fui-checkbox-unchecked"></span>
+									<span class="second-icon fui-checkbox-checked"></span>
+								</span>
+									<input type="checkbox" value="" id="checkbox1" data-toggle="checkbox">
+									<p class="font-10">Full Time</p>
+							</label>
+				        </div>              
+				        <div class="col-md-6">
+							<label class="checkbox" for="checkbox1">
+								<span class="icons">
+									<span class="first-icon fui-checkbox-unchecked"></span>
+									<span class="second-icon fui-checkbox-checked"></span>
+								</span>
+									<input type="checkbox" value="" id="checkbox1" data-toggle="checkbox">
+									<p class="font-10">Part Time</p>
+							</label>
+				        </div>  
+				        <div class="col-md-6">
+							<label class="checkbox" for="checkbox1">
+								<span class="icons">
+									<span class="first-icon fui-checkbox-unchecked"></span>
+									<span class="second-icon fui-checkbox-checked"></span>
+								</span>
+									<input type="checkbox" value="" id="checkbox1" data-toggle="checkbox">
+									<p class="font-10">Piece Work</p>
+							</label>
+				        </div>  
+				        <div class="col-md-6">
+							<label class="checkbox" for="checkbox1">
+								<span class="icons">
+									<span class="first-icon fui-checkbox-unchecked"></span>
+									<span class="second-icon fui-checkbox-checked"></span>
+								</span>
+									<input type="checkbox" value="" id="checkbox1" data-toggle="checkbox">
+									<p class="font-10">Flexie</p>
+							</label>
+				        </div> 
+						<div class="btn-group select select-block mbl">
+							  <button class="green btn dropdown-toggle clearfix btn-info" data-toggle="dropdown">
+							  <span class="filter-option pull-left">Distance</span>&nbsp;<span class="caret"></span>
+							  </button>
+							  <span class="dropdown-arrow"></span>
+							  <ul class="dropdown-menu" role="menu" style="overflow-y: auto; min-height: 108px; max-height: 297px;">
+								  <li rel="0"><a tabindex="-1" href="#" class="opt "><span class="pull-left">10 Miles</span></a></li>
+								  <li rel="1"><a tabindex="-1" href="#" class="opt "><span class="pull-left">25 Miles</span></a></li>
+								  <li rel="2"><a tabindex="-1" href="#" class="opt "><span class="pull-left">50 Miles</span></a></li>
+								  <li rel="2"><a tabindex="-1" href="#" class="opt "><span class="pull-left">70 Miles</span></a></li>
+								  <li rel="2"><a tabindex="-1" href="#" class="opt "><span class="pull-left">90 Miles</span></a></li>
+							  </ul>
+						</div>
+				
+						<a href="#fakelink" class="btn btn-lg btn-block btn-primary ">Search</a>	        				        				        					 
+					  </div><!-------END OF QUICK SEARCH-------->
+			      
+			      
+			      
+			          <div style="height:400px" class="col-md-5 offwhite margin-left-20 border-radius-5 faint-shadow border-white">
+				          <div class="col-md-4 text-center border-bottom-1 border-right-1">
+				          		<p  class="margin-top-10"><a href="#">Current</a></p>
+				          </div>
+				          <div class="col-md-4 text-center border-bottom-1 border-right-1">
+				          		<p  class="margin-top-10"><a href="#">Pending</a></p>
+				          </div>				          
+				          <div class="col-md-4 text-center border-bottom-1">
+				          		<p class="margin-top-10"><a href="#">Complete</a></p>
+				          </div>				          
+				          		<div style="height:80px" class="col-md-12 blue green-hover faint-shadow margin-top-20 white-text">
+				          			<span class="fui-cross d-blue-hover top-right"></span>
+				          			<h6 class="font-20 margin-none ">Tamdown<span class="font-12 margin-left-50">Status - Choosing Applicant</span></h6>
+				          		</div>
+				          		<div style="height:80px" class="col-md-12 blue green-hover faint-shadow margin-top-20 white-text">
+				          			<span class="fui-cross d-blue-hover top-right"></span>
+				          			<h6 class="font-20 margin-none ">Frankis Solutions<span class="font-12 margin-left-50">Status - Choosing Applicant</span></h6>
+				          		</div>
+				          		<div style="height:80px" class="col-md-12 blue green-hover faint-shadow margin-top-20 white-text">
+				          			<span class="fui-cross d-blue-hover top-right"></span>
+				          			<h6 class="font-20 margin-none ">Allianz Plc<span class="font-12 margin-left-50">Status - Choosing Applicant</span></h6>
+				          		</div>
+				          			<a href="#"><p class="font-15 pull-right margin-top-10">View All</p></a>
+					  </div><!-------END OF CURRENT APLICATIONS-------->
+						  	
+						  	
+			          <div style="height:400px" class="col-md-3 offwhite margin-left-20 border-radius-5 faint-shadow border-white">
+				          <div class="col-md-12 text-center margin-top-10 border-bottom-1">
+				         	 <p><span class="fui-mail margin-right-20"></span>Messages</p>
+				          </div>
+				          		<div style="height:80px" class="col-md-12 white faint-shadow green-hover margin-top-20 padding-10">
+				          			<p class="font-15 margin-none">Samuel Gleason</p>
+				          			<p class="font-13">Hi John, please could you come in for an interview...<span class="pull-right font-10">Read more</span></p>
+				          		</div>
+				          		<div style="height:80px" class="col-md-12 white faint-shadow green-hover margin-top-20 padding-10">
+				          			<p class="font-15 margin-none">George Williams</p>
+				          			<p class="font-13">Hi John, please could you come in for an interview...<span class="pull-right font-10">Read more</span></p>
+				          		</div>
+				          		<div style="height:80px" class="col-md-12 white faint-shadow green-hover margin-top-20 padding-10">
+				          			<p class="font-15 margin-none">Sam Mathews</p>
+				          			<p class="font-13">Hi John, please could you come in for an interview...<span class="pull-right font-10">Read more</span></p>
+				          		</div>
+				          			<a href="#"><p class="font-15 pull-right margin-top-10">View All</p></a>
+					 </div><!----------END OF MESSAGES----------->
+			     </div><!----------END OF ROW----------->
+	          
+          </div><!-----------END OF GROUP-CONTAINER  ----------->
+      </div><!-----------END OF CONTAINER  ----------->
+
+
+
+
+
+    <!-- Load JS here for greater good =============================-->
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/jquery-ui-1.10.3.custom.min.js"></script>
+    <script src="js/jquery.ui.touch-punch.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/bootstrap-switch.js"></script>
+    <script src="js/flatui-checkbox.js"></script>
+    <script src="js/flatui-radio.js"></script>
+    <script src="js/jquery.tagsinput.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+  </body>
+</html>
